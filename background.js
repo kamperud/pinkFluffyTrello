f26d8c4de356c78e63d51849aa19bb4476671b22
Hanna Kamperud <hanna.kamperud@gmail.com>
2019-09-27 22:59:25 +0200

    Always on in Trello

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 chrome.runtime.onInstalled.addListener(function() {
     const defaultValue = {
         path: "https://images.unsplash.com/photo-1516642898673-edd1ced08e87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-        description: "pink fluff",
-        active: false
+        description: "pink fluff"
     };
     chrome.storage.sync.set({image: defaultValue.path}, function() {
       console.log("The background image is "+ defaultValue.description);
@@ -17,14 +16,19 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
       });
 
-      chrome.browserAction.onClicked.addListener(function() {
-        chrome.storage.sync.get(['image', 'active'], function(data) {
-            const css= '"url(' + data.image + ')"';
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-              chrome.tabs.executeScript(
-                  tabs[0].id,
-                  {code: 'document.getElementById("trello-root").style.backgroundImage = '+ css +';'});
-            });
-        });
-     });
+     chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+      const url = new URL(tab.url)
+      if (url.hostname !== 'trello.com') return;
+
+      if (changeInfo.status == 'complete' && tab.active) {
+        chrome.storage.sync.get(['image'], function(data) {
+          const css= '"url(' + data.image + ')"';
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {code: 'document.getElementById("trello-root").style.backgroundImage = '+ css +';'});
+          });
+      });
+      }
+    })
   });
