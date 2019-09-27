c880b0f460d4ee80813e582f14a1527d2fdc71b7
Hanna Kamperud <hanna.kamperud@gmail.com>
2019-09-27 22:10:40 +0200

    With popup

--- background.js
+++ background.js
@@ -1,14 +1,22 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+    const defaultValue = {
+        path: "https://images.unsplash.com/photo-1516642898673-edd1ced08e87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+        description: "pink fluff"
+    };
+    chrome.storage.sync.set({image: defaultValue.path}, function() {
+      console.log("The background image is "+ defaultValue.description);
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: {hostEquals: 'trello.com'},
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
       });
   });
+
+//   chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.executeScript(null, {file: "popup.js"});
+//  });
--- popup.js
+++ popup.js
@@ -1,22 +1,8 @@
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-
-
-const pinkFluffPath = 'https://images.unsplash.com/photo-1516642898673-edd1ced08e87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80';
-const pinkFluffUrl = '"url(' + pinkFluffPath + ')"';
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
+chrome.storage.sync.get('image', function(data) {
+    const css= '"url(' + data.image + ')"';
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: 'document.body.style.backgroundImage = '+pinkFluffUrl+';'});
-        //   {code: 'document.body.style.backgroundImage = url("' + pinkFluff + '");'});
-        //   {code: 'document.body.style.backgroundImage = "url("'+pinkFluff+'")";'});
+          {code: 'document.getElementById("trello-root").style.backgroundImage = '+ css +';'});
     });
-};
+});
